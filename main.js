769c3db63c095fb2f0451b020206370ec6a498ce
Maciej Mórawski <murek112@gmail.com>
2020-09-09 22:38:34 +0200

    HTML % CSS formated by BEM, JS DOM

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ console.log(firstName);
 console.log(age);
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
 
-const heading = document.querySelector(".main-heading--js");
+const heading = document.querySelector(".main__heading--js");
 
 heading.innerHTML = `Siema, nazywam się ${firstName} i mam ${age} lat.`;
+
+const emptyParagraph = document.querySelector(".week-summary__description--js");
+
+emptyParagraph.innerHTML = "Nawet uzupełniłem treść javascriptem!";
